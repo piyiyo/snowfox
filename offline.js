3e2c8620da05ac7698354b6d4206d1cd2bbdc24a
piyiyo <35508455+piyiyo@users.noreply.github.com>
2018-01-17 00:30:44 +0100

    Add existing file

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1511192956,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/fox-sheet0.png",
+		"images/fox-sheet1.png",
+		"images/fox-sheet2.png",
+		"images/forest-sheet0.png",
+		"images/snowdrop-sheet0.png",
+		"images/forest2-sheet0.png",
+		"images/forest3-sheet0.png",
+		"media/run.m4a",
+		"media/run.ogg",
+		"media/wind.m4a",
+		"media/wind.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
